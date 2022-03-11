f2603bb0d04efd6f4f106d2ab4b93fb58a3bb5b3
mahmudul1995 <mahmuduls451@gmail.com>
2022-03-11 23:19:38 +0600

    second commit

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 const detailsButton = document.getElementById("detail-submit-btn");
 
+detailsButton.addEventListener("click", function () {
+  const buyerDetails = document.getElementById("buyer-details-input");
+  document.getElementById("buyer-info").innerText = buyerDetails.value;
+  buyerDetails.value = "";
+});
+
+const addProductBtn = document.getElementById("add-details-btn");
+
